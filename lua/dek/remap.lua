162873ee2ae80fedc22f1b6c847577c4ed7d0b2d
Dmitrii Kishchukov <kishchuk@iebdev11.be-md.ncbi.nlm.nih.gov>
2023-02-14 15:42:17 -0500

    .

--- lua/dek/remap.lua
+++ lua/dek/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
+vim.keymap.set("n", "<F6>", vim.cmd.bnext)
+vim.keymap.set("n", "<F5>", vim.cmd.bprevious)
+vim.keymap.set("n", "<F2>", vim.cmd.write)
