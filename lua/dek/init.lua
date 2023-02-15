3e76fa65ae0e0ededd07195acbb1669bac7af273
Dmitrii Kishchukov <grok@grok45.com>
2023-02-15 13:34:02 -0500

    .

--- lua/dek/init.lua
+++ lua/dek/init.lua
@@ -1,1 +1,9 @@
 require("dek.remap")
+--vim.wo.number = true
+
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
+vim.opt.swapfile = false
