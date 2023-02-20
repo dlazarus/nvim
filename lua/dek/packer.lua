045238a8388c8f26c1bd798eebb8473acda39572
grok <grok@a.com>
2023-02-20 12:00:19 -0500

    nerd tree

--- lua/dek/packer.lua
+++ lua/dek/packer.lua
@@ -61,35 +61,16 @@ return require('packer').startup(function(use)
   use( 'hrsh7th/cmp-cmdline' )
   use( 'hrsh7th/nvim-cmp' )
 
-
+  -- tabs
   use {
 	  'akinsho/bufferline.nvim',
 	  tag = 'v3.*'
   }
 
-  --[[
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {'williamboman/mason.nvim'},           -- Optional
-		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+  -- Spell Check. It looks like it does not work. Todo: fix
+  use ('rhysd/vim-grammarous')
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},         -- Required
-		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		  {'hrsh7th/cmp-buffer'},       -- Optional
-		  {'hrsh7th/cmp-path'},         -- Optional
-		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},             -- Required
-		  {'rafamadriz/friendly-snippets'}, -- Optional
-	  }
-  }
-    ]]
+  use ('tpope/vim-commentary')
+  use ('preservim/nerdtree')
 
 end)
